6ea33ed3e0da59322f3cf4b3792171b7b1104370
영건 최 <f2o7891@naver.com>
2024-06-21 16:41:33 +0900

    Operator부분 지금까지 만든거 업로드

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Tb_ServoSpeed = new System.Windows.Forms.TextBox();
+            this.Bt_Stop = new System.Windows.Forms.Button();
+            this.Lb_ServoConnect = new System.Windows.Forms.Label();
+            this.Bt_OpenServo = new System.Windows.Forms.Button();
+            this.Panel_SideMenu = new System.Windows.Forms.Panel();
+            this.CheckBoxHide = new System.Windows.Forms.CheckBox();
+            this.Bt_Start = new System.Windows.Forms.Button();
+            this.Lb_Connect = new System.Windows.Forms.Label();
+            this.Bt_ClosePLC = new System.Windows.Forms.Button();
+            this.Bt_OpenPLC = new System.Windows.Forms.Button();
+            this.Panel_SideMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tb_ServoSpeed
+            // 
+            this.Tb_ServoSpeed.Location = new System.Drawing.Point(224, 24);
+            this.Tb_ServoSpeed.Name = "Tb_ServoSpeed";
+            this.Tb_ServoSpeed.Size = new System.Drawing.Size(144, 25);
+            this.Tb_ServoSpeed.TabIndex = 17;
+            // 
+            // Bt_Stop
+            // 
+            this.Bt_Stop.Enabled = false;
+            this.Bt_Stop.Location = new System.Drawing.Point(376, 80);
+            this.Bt_Stop.Name = "Bt_Stop";
+            this.Bt_Stop.Size = new System.Drawing.Size(144, 48);
+            this.Bt_Stop.TabIndex = 16;
+            this.Bt_Stop.Text = "정지 및 초기화";
+            this.Bt_Stop.UseVisualStyleBackColor = true;
+            this.Bt_Stop.Click += new System.EventHandler(this.Bt_Stop_Click);
+            // 
+            // Lb_ServoConnect
+            // 
+            this.Lb_ServoConnect.Location = new System.Drawing.Point(224, 136);
+            this.Lb_ServoConnect.Name = "Lb_ServoConnect";
+            this.Lb_ServoConnect.Size = new System.Drawing.Size(144, 23);
+            this.Lb_ServoConnect.TabIndex = 15;
+            this.Lb_ServoConnect.Text = "대기중";
+            this.Lb_ServoConnect.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Bt_OpenServo
+            // 
+            this.Bt_OpenServo.Enabled = false;
+            this.Bt_OpenServo.Location = new System.Drawing.Point(224, 56);
+            this.Bt_OpenServo.Name = "Bt_OpenServo";
+            this.Bt_OpenServo.Size = new System.Drawing.Size(144, 48);
+            this.Bt_OpenServo.TabIndex = 14;
+            this.Bt_OpenServo.Text = "서보 준비";
+            this.Bt_OpenServo.UseVisualStyleBackColor = true;
+            this.Bt_OpenServo.Click += new System.EventHandler(this.Bt_OpenServo_Click);
+            // 
+            // Panel_SideMenu
+            // 
+            this.Panel_SideMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Panel_SideMenu.Controls.Add(this.CheckBoxHide);
+            this.Panel_SideMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Panel_SideMenu.Location = new System.Drawing.Point(0, 0);
+            this.Panel_SideMenu.Name = "Panel_SideMenu";
+            this.Panel_SideMenu.Size = new System.Drawing.Size(50, 450);
+            this.Panel_SideMenu.TabIndex = 9;
+            // 
+            // CheckBoxHide
+            // 
+            this.CheckBoxHide.Appearance = System.Windows.Forms.Appearance.Button;
+            this.CheckBoxHide.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.CheckBoxHide.FlatAppearance.BorderSize = 0;
+            this.CheckBoxHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckBoxHide.ForeColor = System.Drawing.Color.White;
+            this.CheckBoxHide.Location = new System.Drawing.Point(0, 392);
+            this.CheckBoxHide.Name = "CheckBoxHide";
+            this.CheckBoxHide.Size = new System.Drawing.Size(50, 58);
+            this.CheckBoxHide.TabIndex = 0;
+            this.CheckBoxHide.Text = ">";
+            this.CheckBoxHide.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CheckBoxHide.UseVisualStyleBackColor = true;
+            this.CheckBoxHide.CheckedChanged += new System.EventHandler(this.CheckBoxHide_CheckedChanged);
+            // 
+            // Bt_Start
+            // 
+            this.Bt_Start.Enabled = false;
+            this.Bt_Start.Location = new System.Drawing.Point(376, 24);
+            this.Bt_Start.Name = "Bt_Start";
+            this.Bt_Start.Size = new System.Drawing.Size(144, 48);
+            this.Bt_Start.TabIndex = 13;
+            this.Bt_Start.Text = "시작";
+            this.Bt_Start.UseVisualStyleBackColor = true;
+            this.Bt_Start.Click += new System.EventHandler(this.Bt_Start_Click);
+            // 
+            // Lb_Connect
+            // 
+            this.Lb_Connect.Location = new System.Drawing.Point(72, 136);
+            this.Lb_Connect.Name = "Lb_Connect";
+            this.Lb_Connect.Size = new System.Drawing.Size(144, 23);
+            this.Lb_Connect.TabIndex = 12;
+            this.Lb_Connect.Text = "대기중";
+            this.Lb_Connect.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Bt_ClosePLC
+            // 
+            this.Bt_ClosePLC.Enabled = false;
+            this.Bt_ClosePLC.Location = new System.Drawing.Point(72, 80);
+            this.Bt_ClosePLC.Name = "Bt_ClosePLC";
+            this.Bt_ClosePLC.Size = new System.Drawing.Size(144, 48);
+            this.Bt_ClosePLC.TabIndex = 11;
+            this.Bt_ClosePLC.Text = "PLC 연결 해제";
+            this.Bt_ClosePLC.UseVisualStyleBackColor = true;
+            this.Bt_ClosePLC.Click += new System.EventHandler(this.Bt_ClosePLC_Click);
+            // 
+            // Bt_OpenPLC
+            // 
+            this.Bt_OpenPLC.Location = new System.Drawing.Point(72, 24);
+            this.Bt_OpenPLC.Name = "Bt_OpenPLC";
+            this.Bt_OpenPLC.Size = new System.Drawing.Size(144, 48);
+            this.Bt_OpenPLC.TabIndex = 10;
+            this.Bt_OpenPLC.Text = "PLC 연결";
+            this.Bt_OpenPLC.UseVisualStyleBackColor = true;
+            this.Bt_OpenPLC.Click += new System.EventHandler(this.Bt_OpenPLC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Panel_SideMenu);
+            this.Controls.Add(this.Tb_ServoSpeed);
+            this.Controls.Add(this.Bt_Stop);
+            this.Controls.Add(this.Lb_ServoConnect);
+            this.Controls.Add(this.Bt_OpenServo);
+            this.Controls.Add(this.Bt_Start);
+            this.Controls.Add(this.Lb_Connect);
+            this.Controls.Add(this.Bt_ClosePLC);
+            this.Controls.Add(this.Bt_OpenPLC);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Panel_SideMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Tb_ServoSpeed;
+        private System.Windows.Forms.Button Bt_Stop;
+        private System.Windows.Forms.Label Lb_ServoConnect;
+        private System.Windows.Forms.Button Bt_OpenServo;
+        private System.Windows.Forms.Panel Panel_SideMenu;
+        private System.Windows.Forms.CheckBox CheckBoxHide;
+        private System.Windows.Forms.Button Bt_Start;
+        private System.Windows.Forms.Label Lb_Connect;
+        private System.Windows.Forms.Button Bt_ClosePLC;
+        private System.Windows.Forms.Button Bt_OpenPLC;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,412 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ActUtlTypeLib;
 
 namespace cnHRD_MES_Project
 {
     public partial class Form1 : Form
     {
+        System.Windows.Forms.Timer Timer_Slide = new System.Windows.Forms.Timer(); //슬라이드용 타이머
+        System.Windows.Forms.Timer Timer_Operation = new System.Windows.Forms.Timer(); //동작용 타이머
+        ActUtlType PLC01 = new ActUtlType();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Timer_Slide.Interval = 10; //슬라이드메뉴
+            Timer_Slide.Tick += new EventHandler(Timer_Sl); //슬라이드 메뉴
+            Timer_Operation.Interval = 100;
+            Timer_Operation.Tick += new EventHandler(Timer_Op); //오퍼레이팅 타이머
+        }
+
+        private void CheckBoxHide_CheckedChanged(object sender, EventArgs e) //슬라이드 메뉴
+        {
+            if (CheckBoxHide.Checked == false)
+            {
+                CheckBoxHide.Text = ">";
+            }
+            else
+            {
+                CheckBoxHide.Text = "<";
+            }
+            Timer_Slide.Start();
+        }
+
+        int Slide_Width = 50; //슬라이드 메뉴
+
+        void Timer_Sl(object sender, EventArgs e) //슬라이드 메뉴
+        {
+            if (CheckBoxHide.Checked == false)
+            {
+                Slide_Width -= 10;
+                if (Slide_Width <= 50)
+                    Timer_Slide.Stop();
+            }
+
+            if (CheckBoxHide.Checked == true)
+            {
+                Slide_Width += 10;
+                if (Slide_Width >= 200)
+                    Timer_Slide.Stop();
+            }
+
+            Panel_SideMenu.Width = Slide_Width;
+        }
+
+        public int PLC_Open; //PLC 연결상태
+
+        private void Bt_OpenPLC_Click(object sender, EventArgs e) //PLC 연결버튼
+        {
+            PLC01.ActLogicalStationNumber = 1;
+            PLC_Open = PLC01.Open();
+            if (PLC_Open == 0)
+            {
+                Lb_Connect.Text = "연결됨";
+                Lb_Connect.ForeColor = Color.Green;
+                Bt_ClosePLC.Enabled = true;
+                Bt_OpenServo.Enabled = true;
+                Bt_OpenPLC.Enabled = false;
+            }
+            else
+            {
+                Lb_Connect.Text = "연결실패 : 0x" + PLC_Open;
+                Lb_Connect.ForeColor = Color.Red;
+            }
+        }
+
+        private void Bt_ClosePLC_Click(object sender, EventArgs e) //PLC 연결종료버튼
+        {
+            PLC01.Close();
+            Lb_Connect.Text = "연결종료";
+            Lb_Connect.ForeColor = Color.Red;
+
+            Bt_OpenPLC.Enabled = true;
+            Bt_OpenServo.Enabled = false;
+            Bt_Start.Enabled = false;
+        }
+
+        private void Bt_Start_Click(object sender, EventArgs e)
+        {
+            int iServoSpeed = (Convert.ToInt32(Tb_ServoSpeed.Text)) * 100;
+
+            ushort[] temp = new ushort[2];
+
+            temp[0] = (ushort)iServoSpeed;
+            temp[1] = (ushort)(iServoSpeed >> 16);
+            Timer_Operation.Start();
+
+            PLC01.WriteBuffer(6, 1518, 1, (short)temp[0]);
+            PLC01.WriteBuffer(6, 1519, 1, (short)temp[1]);
+        }
+
+        private void Bt_OpenServo_Click(object sender, EventArgs e)
+        {
+            PLC01.SetDevice("Y60", 1);
+            PLC01.GetDevice("X60", out int i);
+            PLC01.GetDevice("Y60", out int j);
+
+            if (i == 1 && j == 1)
+            {
+                Lb_ServoConnect.Text = "서보 준비완료";
+                Lb_ServoConnect.ForeColor = Color.Green;
+                Bt_Start.Enabled = true;
+                PLC01.WriteBuffer(6, 1500, 1, 9001);
+                PLC01.SetDevice("Y70", 1);
+            }
+            else
+            {
+                Lb_ServoConnect.Text = "서보 준비실패";
+                Lb_ServoConnect.ForeColor = Color.Red;
+            }
+        }
+
+        private void Bt_Stop_Click(object sender, EventArgs e)
+        {
+            iMode = 0;
+        }
+
+        void Sup_Fwd() //공급전진
+        {
+            PLC01.SetDevice("Y20", 1);
+            PLC01.SetDevice("Y21", 0);
+        }
+
+        void Sup_Bwd() //공급후진
+        {
+            PLC01.SetDevice("Y20", 0);
+            PLC01.SetDevice("Y21", 1);
+        }
+
+        void Trans_Fwd() //송출전진
+        {
+            PLC01.SetDevice("Y22", 1);
+            PLC01.SetDevice("Y23", 0);
+        }
+
+        void Trans_Bwd() //송출후진
+        {
+            PLC01.SetDevice("Y22", 0);
+            PLC01.SetDevice("Y23", 1);
+        }
+
+        void Con_On() //컨베이어정회전
+        {
+            PLC01.SetDevice("Y28", 1);
+            PLC01.SetDevice("Y2F", 0);
+        }
+
+        void Con_Off() //컨베이어정지
+        {
+            PLC01.SetDevice("Y28", 0);
+            PLC01.SetDevice("Y2F", 0);
+        }
+
+        void CCon_On() //컨베이어역회전
+        {
+            PLC01.SetDevice("Y28", 0);
+            PLC01.SetDevice("Y2F", 1);
+        }
+
+        void Out_Fwd() //배출전진
+        {
+            PLC01.SetDevice("Y24", 1);
+            PLC01.SetDevice("Y25", 0);
+        }
+
+        void Out_Bwd() //배출후진
+        {
+            PLC01.SetDevice("Y24", 0);
+            PLC01.SetDevice("Y25", 1);
+        }
+
+        void Stop_Fwd() //스톱전진
+        {
+            PLC01.SetDevice("Y2D", 1);
+        }
+
+        void Stop_Bwd() //스톱후진
+        {
+            PLC01.SetDevice("Y2D", 0);
+        }
+
+        void Comp_Fwd() //흡착전진
+        {
+            PLC01.SetDevice("Y29", 1);
+            PLC01.SetDevice("Y2A", 0);
+        }
+
+        void Comp_Bwd() //흡착후진
+        {
+            PLC01.SetDevice("Y29", 0);
+            PLC01.SetDevice("Y2A", 1);
+        }
+
+        void Ware_Fwd() //창고전진
+        {
+            PLC01.SetDevice("Y2B", 1);
+            PLC01.SetDevice("Y2C", 0);
+        }
+
+        void Ware_Bwd() //창고후진
+        {
+            PLC01.SetDevice("Y2B", 0);
+            PLC01.SetDevice("Y2C", 1);
+        }
+
+        void CompPad_On() //흡착패드온
+        {
+            PLC01.SetDevice("Y2E", 1);
+        }
+
+        void CompPad_Off() //흡착패드오프
+        {
+            PLC01.SetDevice("Y2E", 0);
+        }
+
+        private static bool Check_Bit(int iData, int iLoc) //X를 위한 체크비트
+        {
+            int iVal = (0x1 << iLoc);
+            return ((int)iData & iVal) == iVal;
+        }
+
+        int[] X_DATA = new int[8];
+        public bool[,] X = new bool[8, 16];
+        public int[] Is_Order = { 0, 0, 0, 0, 0 };
+        int iProLoad = 0;
+        int iDeliv = 0;
+        public bool Is_Metal;
+        public int[] Ware_Location = { 0, 0 };
+        bool bStart = true;
+        int iMode = 2;
+        int[] iLocation = { 0, 0, 0, 0, 0 };
+
+        void Is_Load(string Is_Metal, int X, int Y)
+        {
+
+        }
+
+        void Timer_Op(object sender, EventArgs e)
+        {
+            PLC01.ReadDeviceBlock("X0", 2, out X_DATA[0]);
+
+            //x차원 배열에 값을 저장
+            for (int ct1 = 0; ct1 <= 1; ct1++)
+            {
+                for (int ct2 = 0; ct2 <= 15; ct2++)
+                {
+                    X[ct1, ct2] = Check_Bit(X_DATA[ct1], ct2);
+                }
+            }
+
+            if (iMode == 0) //비상정지
+            {
+                Bt_Start.Enabled = false;
+                Sup_Bwd();
+                Trans_Bwd();
+                Con_Off();
+                Ware_Bwd();
+                Stop_Bwd();
+                Out_Bwd();
+                Comp_Bwd();
+                CompPad_Off();
+                Task.Delay(2000);
+                Timer_Operation.Stop();
+                bStart = true;
+                Bt_Start.Enabled = true;
+                iMode = 2;
+            }
+
+            else if (bStart == true)
+            {
+                if (Is_Order[0] == 1) //주문이 있다면
+                {
+                    iMode = 1;
+                    iLocation = Is_Order;
+                }
+                else if (Is_Order[0] == 0) //주문이 없다면
+                {
+                    iMode = 2;
+                    iLocation[2] = Ware_Location[0];
+                    iLocation[3] = Ware_Location[1];
+                }
+                iProLoad = 0;
+                iDeliv = 0;
+                bStart = false;
+            }
+
+            else if (iMode == 1)
+            {
+                switch (iDeliv)
+                {
+                    case 0: //1위치 서보이동
+                        short Ware_Loc = (short)(Ware_Location[1] * 2 + 1); //1,3,5
+                        PLC01.WriteBuffer(6, 1500, 1, Ware_Loc);
+                        PLC01.SetDevice("Y70", 1);
+                        if (Ware_Location[0] == 0) //창고이동
+                            Ware_Fwd();
+                        else
+                            Ware_Bwd();
+                        iDeliv++;
+                        break;
+                }
+            }
+
+            else if (iMode == 2) //주문이 없다면
+            {
+                switch (iProLoad)
+                {
+                    case 0: //공급전진 if 공급전(X10)까지
+                        if (X[0, 8] && X[1, 1])
+                            Sup_Fwd();
+                        if (X[1, 0] == true)
+                            iProLoad++; break;
+                    case 1: //공급후진 if 공급후(X11)까지
+                        Sup_Bwd();
+                        if (X[1, 1] == true)
+                            iProLoad++; break;
+                    case 2: //송출전진 if 송출전(X14)까지
+                        Trans_Fwd();
+                        if (X[1, 4] == true)
+                            iProLoad++; break;
+                    case 3: //송출후진, 컨구동 if 용량형(X0A)까지
+                        Trans_Bwd(); Con_On();
+                        if (X[0, 10] == true)
+                            iProLoad++; break;
+                    case 4: //금속판별, 스톱다운
+                        Stop_Fwd();
+                        if (X[0, 9] == true) //고주파센서 감지
+                            Is_Metal = true;
+                        else //고주파센서 감지x
+                            Is_Metal = false;
+                        iProLoad++; break;
+                    case 5: //if 스토퍼(X0B) - 1위치 서보이동
+                        if (X[0, 11] == true)
+                        {
+                            PLC01.WriteBuffer(6, 1500, 1, 7);
+                            PLC01.SetDevice("Y70", 1);
+                            iProLoad++;
+                        }
+                        break;
+                    case 6: //if 컨위치 이동완료 - 스톱업, 컨정지, 흡착온 - 1위치 서보이동
+                        PLC01.GetDevice("X74", out int i);
+                        if (i == 1)
+                        {
+                            Stop_Bwd();
+                            Con_Off();
+                            CompPad_On();
+                            Task.Delay(500); //딜레이 0.5초
+                            short Ware_Loc = (short)(Ware_Location[1] * 2 + 1); //1,3,5
+                            PLC01.WriteBuffer(6, 1500, 1, Ware_Loc);
+                            PLC01.SetDevice("Y70", 1);
+                            if (Ware_Location[0] == 0) //창고이동
+                                Ware_Fwd();
+                            else
+                                Ware_Bwd();
+                            iProLoad++;
+                        }
+                        break;
+                    case 7: //if 1위치 이동완료 - 흡착전진 if 흡착전(X1A)까지 - 2위치 서보이동
+                        PLC01.GetDevice("X74", out i);
+                        if (i == 1)
+                            Comp_Fwd();
+                        if (X[1, 10] == true)
+                        {
+                            short Ware_Loc = (short)((Ware_Location[1] + 1) * 2); //2,4,6
+                            PLC01.WriteBuffer(6, 1500, 1, Ware_Loc);
+                            PLC01.SetDevice("Y70", 1);
+                            iProLoad++;
+                        }
+                        break;
+                    case 8: //if 2위치 이동완료 - 흡착오프 - 1위치 재이동
+                        PLC01.GetDevice("X74", out i);
+                        if (i == 1)
+                        {
+                            CompPad_Off();
+                            Task.Delay(500); //딜레이 0.5초
+                            short Ware_Loc = (short)(Ware_Location[1] * 2 + 1); //1,3,5
+                            PLC01.WriteBuffer(6, 1500, 1, Ware_Loc);
+                            PLC01.SetDevice("Y70", 1);
+                            iProLoad++;
+                        }
+                        break;
+                    case 9: //if 1위치 재이동완료 - 흡착후진 - if흡착후까지
+                        PLC01.GetDevice("X74", out i);
+                        if (i == 1)
+                            Comp_Bwd();
+                        if (X[1, 11] == true)
+                        {
+                            if (Is_Metal == true) Is_Load("금속", Ware_Location[0], Ware_Location[1]);
+                            else if (Is_Metal == false) Is_Load("비금속", Ware_Location[0], Ware_Location[1]);
+                            bStart = true;
+                        }
+                        break;
+                }
+            }
         }
     }
 }
