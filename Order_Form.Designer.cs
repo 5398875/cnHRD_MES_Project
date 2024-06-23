05924a7e5b60aebfcecfad3eac7fa5e0305530a8
DHK <kimdh3273@gmail.com>
2024-06-23 19:37:42 +0900

    Feat: Order_Form 추가

---
+++ Order_Form.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace cnHRD_MES_Project
+{
+    partial class Order_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Order_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Order_Form";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Order_Form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Order_Form.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cnHRD_MES_Project
+{
+    public partial class Order_Form : Form
+    {
+        public Order_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Order_Form_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
