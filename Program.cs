5c7b399de378ecf8f8f817fc94a4328c22da2fe0
DHK <kimdh3273@gmail.com>
2024-06-24 14:23:52 +0900

    Docs: 폼 준완성, 인수 통신 필요
    대부분 기능 구현 완료.
    *isOrder 반환값은 선언완료
    *but 수신변수 선언 필요
    
    #1a

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace cnHRD_MES_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Operator());
+            Application.Run(new Order_Form());
         }
     }
 }
