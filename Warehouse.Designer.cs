2cabf8c05f7f18932eed7578a29bbd8511584de9
영건 최 <f2o7891@naver.com>
2024-07-04 15:13:06 +0900

    코멘트 추가

--- Warehouse.Designer.cs
+++ Warehouse.Designer.cs
@@ -28,192 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.lb32 = new System.Windows.Forms.Label();
-            this.lb22 = new System.Windows.Forms.Label();
-            this.lb12 = new System.Windows.Forms.Label();
-            this.lb31 = new System.Windows.Forms.Label();
-            this.lb21 = new System.Windows.Forms.Label();
-            this.lb11 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.pb_DockMonitor = new System.Windows.Forms.PictureBox();
-            this.groupBox1.SuspendLayout();
+            this.lb11 = new System.Windows.Forms.PictureBox();
+            this.lb12 = new System.Windows.Forms.PictureBox();
+            this.lb21 = new System.Windows.Forms.PictureBox();
+            this.lb22 = new System.Windows.Forms.PictureBox();
+            this.lb31 = new System.Windows.Forms.PictureBox();
+            this.lb32 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBoxMetal = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNMetal = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.labelMetal = new System.Windows.Forms.Label();
+            this.labelNMetal = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pb_DockMonitor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb31)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb32)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMetal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNMetal)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.lb32);
-            this.groupBox1.Controls.Add(this.lb22);
-            this.groupBox1.Controls.Add(this.lb12);
-            this.groupBox1.Controls.Add(this.lb31);
-            this.groupBox1.Controls.Add(this.lb21);
-            this.groupBox1.Controls.Add(this.lb11);
-            this.groupBox1.Controls.Add(this.label6);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.pb_DockMonitor);
-            this.groupBox1.Location = new System.Drawing.Point(216, 8);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(416, 432);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "창고 적재 현황";
-            // 
-            // lb32
-            // 
-            this.lb32.AutoSize = true;
-            this.lb32.Location = new System.Drawing.Point(288, 96);
-            this.lb32.Name = "lb32";
-            this.lb32.Size = new System.Drawing.Size(0, 22);
-            this.lb32.TabIndex = 1;
-            // 
-            // lb22
-            // 
-            this.lb22.AutoSize = true;
-            this.lb22.Location = new System.Drawing.Point(288, 224);
-            this.lb22.Name = "lb22";
-            this.lb22.Size = new System.Drawing.Size(0, 22);
-            this.lb22.TabIndex = 1;
-            // 
-            // lb12
-            // 
-            this.lb12.AutoSize = true;
-            this.lb12.Location = new System.Drawing.Point(288, 352);
-            this.lb12.Name = "lb12";
-            this.lb12.Size = new System.Drawing.Size(0, 22);
-            this.lb12.TabIndex = 1;
-            // 
-            // lb31
-            // 
-            this.lb31.AutoSize = true;
-            this.lb31.Location = new System.Drawing.Point(80, 96);
-            this.lb31.Name = "lb31";
-            this.lb31.Size = new System.Drawing.Size(0, 22);
-            this.lb31.TabIndex = 1;
-            // 
-            // lb21
-            // 
-            this.lb21.AutoSize = true;
-            this.lb21.Location = new System.Drawing.Point(80, 224);
-            this.lb21.Name = "lb21";
-            this.lb21.Size = new System.Drawing.Size(0, 22);
-            this.lb21.TabIndex = 1;
-            // 
-            // lb11
-            // 
-            this.lb11.AutoSize = true;
-            this.lb11.Location = new System.Drawing.Point(80, 352);
-            this.lb11.Name = "lb11";
-            this.lb11.Size = new System.Drawing.Size(0, 22);
-            this.lb11.TabIndex = 1;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(216, 48);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(49, 22);
-            this.label6.TabIndex = 1;
-            this.label6.Text = "3, 2";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 48);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 22);
-            this.label5.TabIndex = 1;
-            this.label5.Text = "3, 1";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 176);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(49, 22);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "2, 1";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(216, 176);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 22);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "2, 2";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(216, 304);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 22);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "1, 2";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 304);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 22);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "1, 1";
             // 
             // pb_DockMonitor
             // 
-            this.pb_DockMonitor.Location = new System.Drawing.Point(0, 32);
+            this.pb_DockMonitor.Location = new System.Drawing.Point(24, 88);
             this.pb_DockMonitor.Name = "pb_DockMonitor";
-            this.pb_DockMonitor.Size = new System.Drawing.Size(413, 389);
+            this.pb_DockMonitor.Size = new System.Drawing.Size(416, 389);
             this.pb_DockMonitor.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pb_DockMonitor.TabIndex = 0;
             this.pb_DockMonitor.TabStop = false;
+            // 
+            // lb11
+            // 
+            this.lb11.Location = new System.Drawing.Point(96, 360);
+            this.lb11.Name = "lb11";
+            this.lb11.Size = new System.Drawing.Size(104, 80);
+            this.lb11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lb11.TabIndex = 1;
+            this.lb11.TabStop = false;
+            // 
+            // lb12
+            // 
+            this.lb12.Location = new System.Drawing.Point(256, 360);
+            this.lb12.Name = "lb12";
+            this.lb12.Size = new System.Drawing.Size(104, 80);
+            this.lb12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lb12.TabIndex = 1;
+            this.lb12.TabStop = false;
+            // 
+            // lb21
+            // 
+            this.lb21.Location = new System.Drawing.Point(96, 192);
+            this.lb21.Name = "lb21";
+            this.lb21.Size = new System.Drawing.Size(104, 80);
+            this.lb21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lb21.TabIndex = 1;
+            this.lb21.TabStop = false;
+            // 
+            // lb22
+            // 
+            this.lb22.Location = new System.Drawing.Point(256, 192);
+            this.lb22.Name = "lb22";
+            this.lb22.Size = new System.Drawing.Size(104, 80);
+            this.lb22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lb22.TabIndex = 1;
+            this.lb22.TabStop = false;
+            // 
+            // lb31
+            // 
+            this.lb31.Location = new System.Drawing.Point(96, 24);
+            this.lb31.Name = "lb31";
+            this.lb31.Size = new System.Drawing.Size(104, 80);
+            this.lb31.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lb31.TabIndex = 1;
+            this.lb31.TabStop = false;
+            // 
+            // lb32
+            // 
+            this.lb32.Location = new System.Drawing.Point(256, 24);
+            this.lb32.Name = "lb32";
+            this.lb32.Size = new System.Drawing.Size(104, 80);
+            this.lb32.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.lb32.TabIndex = 1;
+            this.lb32.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(113)))), ((int)(((byte)(129)))));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 80);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "창고 적재 현황";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(152, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(360, 520);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.lb11);
+            this.panel2.Controls.Add(this.lb32);
+            this.panel2.Controls.Add(this.lb12);
+            this.panel2.Controls.Add(this.lb31);
+            this.panel2.Controls.Add(this.lb21);
+            this.panel2.Controls.Add(this.lb22);
+            this.panel2.Controls.Add(this.pb_DockMonitor);
+            this.panel2.Location = new System.Drawing.Point(592, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(464, 520);
+            this.panel2.TabIndex = 4;
+            // 
+            // pictureBoxMetal
+            // 
+            this.pictureBoxMetal.Location = new System.Drawing.Point(16, 32);
+            this.pictureBoxMetal.Name = "pictureBoxMetal";
+            this.pictureBoxMetal.Size = new System.Drawing.Size(104, 80);
+            this.pictureBoxMetal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxMetal.TabIndex = 3;
+            this.pictureBoxMetal.TabStop = false;
+            // 
+            // pictureBoxNMetal
+            // 
+            this.pictureBoxNMetal.Location = new System.Drawing.Point(16, 32);
+            this.pictureBoxNMetal.Name = "pictureBoxNMetal";
+            this.pictureBoxNMetal.Size = new System.Drawing.Size(104, 80);
+            this.pictureBoxNMetal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxNMetal.TabIndex = 3;
+            this.pictureBoxNMetal.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(218)))), ((int)(((byte)(225)))));
+            this.panel3.Controls.Add(this.labelMetal);
+            this.panel3.Controls.Add(this.pictureBoxMetal);
+            this.panel3.Location = new System.Drawing.Point(24, 144);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(312, 144);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(218)))), ((int)(((byte)(225)))));
+            this.panel4.Controls.Add(this.labelNMetal);
+            this.panel4.Controls.Add(this.pictureBoxNMetal);
+            this.panel4.Location = new System.Drawing.Point(24, 336);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(312, 144);
+            this.panel4.TabIndex = 4;
+            // 
+            // labelMetal
+            // 
+            this.labelMetal.Location = new System.Drawing.Point(136, 32);
+            this.labelMetal.Name = "labelMetal";
+            this.labelMetal.Size = new System.Drawing.Size(152, 80);
+            this.labelMetal.TabIndex = 4;
+            this.labelMetal.Text = "금속 0개";
+            this.labelMetal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelNMetal
+            // 
+            this.labelNMetal.Location = new System.Drawing.Point(136, 32);
+            this.labelNMetal.Name = "labelNMetal";
+            this.labelNMetal.Size = new System.Drawing.Size(152, 80);
+            this.labelNMetal.TabIndex = 4;
+            this.labelNMetal.Text = "비금속 0개";
+            this.labelNMetal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Warehouse
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Honeydew;
-            this.ClientSize = new System.Drawing.Size(982, 453);
-            this.Controls.Add(this.groupBox1);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(218)))), ((int)(((byte)(225)))));
+            this.ClientSize = new System.Drawing.Size(1253, 570);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("굴림", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "Warehouse";
             this.Text = "Warehouse Monitor";
             this.Load += new System.EventHandler(this.Warehouse_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pb_DockMonitor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb31)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lb32)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMetal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNMetal)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.PictureBox pb_DockMonitor;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox lb11;
+        private System.Windows.Forms.PictureBox lb12;
+        private System.Windows.Forms.PictureBox lb21;
+        private System.Windows.Forms.PictureBox lb22;
+        private System.Windows.Forms.PictureBox lb31;
+        private System.Windows.Forms.PictureBox lb32;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lb11;
-        private System.Windows.Forms.Label lb12;
-        private System.Windows.Forms.Label lb32;
-        private System.Windows.Forms.Label lb22;
-        private System.Windows.Forms.Label lb31;
-        private System.Windows.Forms.Label lb21;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBoxNMetal;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label labelMetal;
+        private System.Windows.Forms.PictureBox pictureBoxMetal;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelNMetal;
     }
 }
--- Warehouse.cs
+++ Warehouse.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,7 +25,25 @@ namespace cnHRD_MES_Project
 
         private void Warehouse_Load(object sender, EventArgs e)//폼 실행시,
         {
-           // pb_DockMonitor.Image = Image.FromFile(System.Environment.CurrentDirectory + "/images/loading dock.png");
+            pb_DockMonitor.Image = Image.FromFile(System.Environment.CurrentDirectory + "/images/loading dock.png");
+            pictureBoxMetal.Image = Image.FromFile(System.Environment.CurrentDirectory + "/images/Metal.png");
+            pictureBoxNMetal.Image = Image.FromFile(System.Environment.CurrentDirectory + "/images/Non-Metal.png");
+            Round(label1, 12);
+            Round(panel1, 36);
+            Round(panel2, 36);
+            Round(panel3, 36);
+            Round(panel4, 36);
+        }
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect,
+            int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        private void Round(Control c, int i)
+        {
+            if (i == 0) c.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, c.Width, c.Height, c.Height, c.Height));
+            else if (i == 1) c.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, c.Width, c.Height, c.Width, c.Width));
+            else c.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, c.Width, c.Height, i, i));
         }
 
         private int[] FindLocationXY(int Type)
@@ -63,18 +83,44 @@ namespace cnHRD_MES_Project
             return WLocation;
         }
 
+        int Metal = 0;
+        int NonMetal = 0;
+
         public void Is_Load(int Is_Metal, int X, int Y) //동작부에서 넘긴 창고 적재 위치저장 함수
         {
             if (X >= 0 && X < WH_Location.GetLength(0) && Y >= 0 && Y < WH_Location.GetLength(1))
             {
                 WH_Location[X, Y] = Is_Metal; //주어진 위치에 금속, 비금속 값 저장
                 Update_Type(X + 1, Y + 1, WH_Location[X, Y]);   //위치에 저장된 종류 업데이트
+                if (Is_Metal == 1)
+                {
+                    Metal++;
+                    labelMetal.Text = "금속 " + Metal.ToString() + "개";
+;                }
+                else if (Is_Metal == 2)
+                {
+                    NonMetal++;
+                    labelNMetal.Text = "비금속 " + NonMetal.ToString() + "개";
+                    ;
+                }
             }
         }
 
         public void Take_From(int X, int Y) //동작부에서 창고 적재물 추출 시 해당하는 창고 위치정보 클리어
         {
-                WH_Location[X, Y] = 0; //주어진 위치의 금속,비금속 제거
+            if (WH_Location[X, Y]  == 1)
+            {
+                Metal--;
+                labelMetal.Text = "금속 " + Metal.ToString() + "개";
+                ;
+            }
+            else if (WH_Location[X, Y] == 2)
+            {
+                NonMetal--;
+                labelNMetal.Text = "비금속 " + NonMetal.ToString() + "개";
+                ;
+            }
+            WH_Location[X, Y] = 0; //주어진 위치의 금속,비금속 제거
                 Update_Type(X + 1, Y + 1, 0);  //위치에서 추출해간 상품 라벨에서 제거
         }
 
@@ -86,24 +132,22 @@ namespace cnHRD_MES_Project
             Control[] controls = this.Controls.Find(lbName, true);   
             //this.control.find 내장함수 이용, lbname과 같은 이름을 가진 컨트롤 controls 생성 및 반환
 
-            if (controls[0] is Label lb)  //찾은 controls가 라벨 타입이라면,
+            if (controls[0] is PictureBox lb)  //찾은 controls가 라벨 타입이라면,
             {
                 switch (Type)   //case문으로 받은 종류 한글 네이밍
                 {
                     case 0:
-                        sType = "비어 있음";
+                        lb.Image = Image.FromFile(System.Environment.CurrentDirectory + "/images/Empty.png");
                         break;
 
                     case 1:
-                        sType = "금속";
+                        lb.Image = Image.FromFile(System.Environment.CurrentDirectory + "/images/Metal.png");
                         break;
 
                     case 2:
-                        sType = "비금속";
+                        lb.Image = Image.FromFile(System.Environment.CurrentDirectory + "/images/Non-Metal.png");
                         break;
                 }
-
-                lb.Text = sType;    //한글 네이밍한 종류를 각 라벨 텍스트에 표시
             }
         }
     }
